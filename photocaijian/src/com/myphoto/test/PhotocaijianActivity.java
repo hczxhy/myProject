786b367a8ff56b34b01d735e55fde0bfe663aa0c
hczxhy <hczxhy@sina.cn>
2016-03-03 17:41:23 +0800

    添加注释

--- photocaijian/src/com/myphoto/test/PhotocaijianActivity.java
+++ photocaijian/src/com/myphoto/test/PhotocaijianActivity.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+/**
+*裁图
+*
+**/
 public class PhotocaijianActivity extends Activity {
     /** Called when the activity is first created. */
     
